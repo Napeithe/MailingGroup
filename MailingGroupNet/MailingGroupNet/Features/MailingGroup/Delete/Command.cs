b3fd2718e9d96d7f0b6999164f1ac8a5eafb8e5b
Napeithe <stolowskip@gmail.com>
2020-02-03 20:28:55 +0100

    delete mailing group

---
+++ MailingGroupNet/MailingGroupNet/Features/MailingGroup/Delete/Command.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MailingGroupNet.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Model.Database;
+using Model.Dto;
+
+namespace MailingGroupNet.Features.MailingGroup.Delete
+{
+    public class Command : IRequest<ApiResult>, IUserRequest
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+    }
+
+    public class Handler :IRequestHandler<Command, ApiResult>
+    {
+        private readonly MailingGroupContext _context;
+
+        public Handler(MailingGroupContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ApiResult> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var mailingGroup = await _context.MailingGroups
+                .Where(x => x.Id == request.Id)
+                .Where(x => x.UserId == request.UserId).FirstOrDefaultAsync(cancellationToken);
+
+            if (mailingGroup is null)
+            {
+                return ApiResult<MailingGroupDto>.Failed("Mail group not exists", 404);
+            }
+
+            _context.Remove(mailingGroup);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return ApiResult.Success();
+        }
+    }
+}
---
+++ MailingGroupNet/MailingGroupNetTest/Features/MailingGroupTests/DeleteTest.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Model.Database;
+using Model.Entity;
+using MailingGroupNet.Features.MailingGroup.Delete;
+using Model.Dto;
+using Xunit;
+
+namespace MailingGroupNetTest.Features.MailingGroupTests
+{
+    public class DeleteTest
+    {
+        [Fact]
+        public async Task ExecuteShouldReturnFailedWhenGroupIsForOtherUser()
+        {
+            //Arrange
+            MailingGroupContext context = ContextBuilder.BuildClean();
+            AppUser firstUser = new AppUser()
+            {
+                UserName = "first",
+                Email = "first@email.com"
+            };
+            AppUser secondUser = new AppUser()
+            {
+                UserName = "second",
+                Email = "second@email.com"
+            };
+            context.Add(firstUser);
+            context.Add(secondUser);
+
+            List<MailingGroup> mailingGroups = new List<MailingGroup>
+            {
+                new MailingGroup()
+                {
+                    Name = "first",
+                    User = firstUser
+                },
+                new MailingGroup()
+                {
+                    Name = "second",
+                    User = secondUser
+                }
+            };
+            context.AddRange(mailingGroups);
+            context.SaveChanges();
+            Command query = new Command()
+            {
+                Id = 2,
+                UserId = firstUser.Id
+            };
+            //Act
+            ApiResult result = await new Handler(context).Handle(query, CancellationToken.None);
+            //Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Message.Should().Be("Mail group not exists");
+            result.StatusCode.Should().Be(404);
+        }
+
+        [Fact]
+        public async Task ExecuteShouldReturnFailedWhenGroupIdIsWrong()
+        {
+            //Arrange
+            MailingGroupContext context = ContextBuilder.BuildClean();
+            AppUser firstUser = new AppUser()
+            {
+                UserName = "first",
+                Email = "first@email.com"
+            };
+            AppUser secondUser = new AppUser()
+            {
+                UserName = "second",
+                Email = "second@email.com"
+            };
+            context.Add(firstUser);
+            context.Add(secondUser);
+
+            List<MailingGroup> mailingGroups = new List<MailingGroup>
+            {
+                new MailingGroup()
+                {
+                    Name = "first",
+                    User = firstUser
+                },
+                new MailingGroup()
+                {
+                    Name = "second",
+                    User = secondUser
+                }
+            };
+            context.AddRange(mailingGroups);
+            context.SaveChanges();
+            Command query = new Command()
+            {
+                Id = 22,
+                UserId = firstUser.Id
+            };
+            //Act
+            ApiResult result = await new Handler(context).Handle(query, CancellationToken.None);
+            //Assert
+            result.IsSuccess.Should().BeFalse();
+            result.Message.Should().Be("Mail group not exists");
+            result.StatusCode.Should().Be(404);
+        }
+
+        [Fact]
+        public async Task ExecuteShouldReturnSuccessAndRemoveMailingGroupWithEmail()
+        {
+            //Arrange
+            MailingGroupContext context = ContextBuilder.BuildClean();
+            AppUser firstUser = new AppUser()
+            {
+                UserName = "first",
+                Email = "first@email.com"
+            };
+            context.Add(firstUser);
+
+            MailingGroup group = new MailingGroup()
+            {
+                Name = "first",
+                User = firstUser,
+            };
+            group.Emails = GenerateEmails(20, group).ToList();
+            context.Add(group);
+            context.SaveChanges();
+            Command query = new Command()
+            {
+                Id = 1,
+                UserId = firstUser.Id
+            };
+            //Act
+            ApiResult result = await new Handler(context).Handle(query, CancellationToken.None);
+            //Assert
+            result.IsSuccess.Should().BeTrue();
+            result.Message.Should().BeNullOrEmpty();
+            result.StatusCode.Should().Be(200);
+
+            context.MailingGroups.Should().BeEmpty();
+            context.Emails.Should().BeEmpty();
+        }
+
+        private IEnumerable<Email> GenerateEmails(int numberOfEmails, MailingGroup mailingGroup)
+        {
+            for (int i = 0; i < numberOfEmails; i++)
+            {
+                yield return new Email()
+                {
+                    Name = $"Email@{i}.pl",
+                    MailingGroup = mailingGroup
+                };
+            }
+        }
+    }
+}
